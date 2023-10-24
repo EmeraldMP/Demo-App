acc78e4fdab26142d1ed4f909c2a58e8da3ddeac
Pacheco Melanie <melanie.pacheco@student-cs.fr>
2023-10-24 14:35:05 +0200

    ADD code

--- index.ts
+++ index.ts
@@ -1,28 +1,15 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
 async function handler(_req: Request): Promise<Response> {
-//     // get request body
-//     const body = _req;
-//   return new Response(JSON.stringify(body));
+    // get request body
+    const word1 = "hello";
+    const word2 = "World";
+    const result = await similarity(word1, word2);
+    return new Response(result)
+}
 
-    const body = {
-        sim1: word1,
-        sim2: word2,
-        lang: "fr",
-        type: "General Word2Vec",
-    };
-    const similarityResponse = await fetch(
-        "http://nlp.polytechnique.fr/similarityscore",
-        {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(body),
-        }
-    );
-    const similarityResponseJson = await similarityResponse.json();
-    return Number(similarityResponseJson.simscore);
+async function similarity(word1, word2){
+    return word1 + word2;
 }
 
 serve(handler);
