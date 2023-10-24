0deb887aea49f28d938a8cc322fe95d918a0e7d4
Pacheco Melanie <melanie.pacheco@student-cs.fr>
2023-10-24 14:37:29 +0200

    ADD code

--- index.ts
+++ index.ts
@@ -3,9 +3,9 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 async function handler(_req: Request): Promise<Response> {
     // get request body
     const word1 = "hello";
-    const word2 = "World";
+    const word2 = "world";
     const result = await similarity(word1, word2);
-    return new Response(result)
+    return new Response(String(result))
 }
 
 async function similarity(word1, word2){
